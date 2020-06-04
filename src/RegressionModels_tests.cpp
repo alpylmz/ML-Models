3ec0ff1e1395c2cf8f57b196cbc77031ef73cef2
alpylmz <ylmz.alp.e@gmail.com>
2020-06-04 16:01:12 +0300

    Update Makefile

--- src/RegressionModels_tests.cpp
+++ src/RegressionModels_tests.cpp
@@ -4,8 +4,9 @@ int main(){
     std::vector<std::vector<double>> data1={{0},{1},{2},{3},{-1},{0},{-1.2}};
     std::vector<std::vector<double>> outputs1={{0},{1},{2},{3},{0.5},{0.2},{-1.5}};
 
+    Matrix matrix1(data1);
     //TEST 1
-    Linear_Regression model1(data1,outputs1,"x","empty");
+    Linear_Regression model1(matrix1,outputs1,Model::model_names::x,std::make_pair(Model::regularization_type::real,0));
     std::vector<std::vector<double>> x1={{100}};
     std::cout << model1.Predict(x1);
 }
